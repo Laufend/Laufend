68e29feb908ddfffa40660d57b630d348e51aa06
Dmitriy Frolov <Laufend86@mail.ru>
2022-05-07 21:48:59 +0300

    Initial commit

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,26 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    private final static UserService userService = new UserServiceImpl();
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Efimov", (byte) 19);
+        userService.saveUser("Petr", "Ivanov", (byte) 26);
+        userService.saveUser("Alexei", "Romanov", (byte) 32);
+        userService.saveUser("Dmitriy", "Fro", (byte) 35);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
+    }
+}
+
---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,22 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/myusersbase";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "930D2bf6";
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    // реализуйте настройку соеденения с БД
+}
